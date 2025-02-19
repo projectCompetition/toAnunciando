e78cca909d1529202a4286594640390775cb8574
andre <notlookingforbills@gmail.com>
2025-02-18 22:11:35 -0300

    Adição do React no projeto

--- project/backend/src/app.module.ts
+++ project/backend/src/app.module.ts
@@ -16,7 +16,7 @@ import { CarroAcessoriosModule } from './modules/carro-acessorios.module';
       port: 5432,
       username: 'postgres',
       password: 'masterkey',
-      database: 'toAnunciando',
+      database: 'postgres',
       autoLoadEntities: true,
       synchronize: true, // Set to false in production
     }),
