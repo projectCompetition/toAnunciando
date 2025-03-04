595b005becdc1d7d6227450af34de94eab17df7d
andre <notlookingforbills@gmail.com>
2025-03-04 19:26:11 -0300

    Implementação da homePage Pública

--- project/frontend/src/AuthRoutes.tsx
+++ project/frontend/src/AuthRoutes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Cadastro from "./components/Cadastro";
+import HomePage from "./publico/HomePage";
 
 const AuthRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/homePage" element={<HomePage />} />
       <Route path="*" element={<Login />} /> {/* Se não encontrar, redireciona para Login */}
     </Routes>
   );
