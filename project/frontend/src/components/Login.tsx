307efd9364e6dd3610f4cb7521ba0d5827f5ec3b
andre <notlookingforbills@gmail.com>
2025-04-14 21:14:37 -0300

    Melhoria nos styles

--- project/frontend/src/components/Login.tsx
+++ project/frontend/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext"; // Importa o contexto de autenticação
+import { useAuth } from "../contexts/AuthContext";
 import api from "../api";
 import "../styles/Login.css";
 import "../styles/Default.css";
@@ -9,7 +9,7 @@ const Login: React.FC = () => {
   const [login, setLogin] = useState("");
   const [senha, setSenha] = useState("");
   const [error, setError] = useState("");
-  const { login: authLogin } = useAuth(); // Usa a função de login do contexto
+  const { login: authLogin } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,9 +22,8 @@ const Login: React.FC = () => {
       const response = await api.post("/auth/login", { login, senha });
 
       if (response.data && response.data.accessToken) {
-        // Chama a função de login do contexto com o token e o nome do anunciante
         authLogin(response.data.accessToken, response.data.anunciante.nome);
-        navigate("/homepage"); // Redireciona para o dashboard
+        navigate("/homepage");
       } else {
         setError("Erro ao fazer login. Tente novamente.");
       }
@@ -35,25 +34,28 @@ const Login: React.FC = () => {
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      handleLogin(); // Chama o login ao pressionar Enter
+      handleLogin();
     }
   };
 
   return (
     <div className="body-login">
       <div className="login-container">
-        {/* Left Section */}
         <div className="login-box">
-          <h1>Bem Vindo!</h1>
+
+          <h1 className="padding-10; display-flex-center" >Bem Vindo!</h1>
+
           {error && <p className="error-message">{error}</p>}
+
           <input
             type="text"
-            placeholder="Login"
+            placeholder="E-mail, CPF ou CNPJ"
             value={login}
             onChange={(e) => setLogin(e.target.value)}
             className="input-field"
             onKeyDown={handleKeyPress} // Detecta pressionamento de tecla
           />
+
           <input
             type="password"
             placeholder="Senha"
@@ -62,12 +64,18 @@ const Login: React.FC = () => {
             className="input-field"
             onKeyDown={handleKeyPress} // Detecta pressionamento de tecla
           />
-          <div className="login-options">
-            <a href="/publico/cadastro">Registre-se</a>
+
+          <div className="login-options justify-content-right">
+            <a href="/login">Esqueci minha senha</a>
           </div>
+
           <button className="button-width-100" onClick={handleLogin}>
-            Sign in
+            Login
           </button>
+
+          <div className="login-options">
+            <p>Novo por aqui? <a href="/publico/cadastro">Cadastre-se</a></p>
+          </div>
         </div>
       </div>
     </div >
